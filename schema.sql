4c5b74e8d476b4360f3c416f90020f4b8dc74dc0
Petr Kramar <kramar@masquerade.cz>
2014-09-07 17:17:00 +1200

    schema update

--- schema.sql
+++ schema.sql
@@ -1,35 +1,42 @@
+drop database `fockity_dbunit`;
+create database if not exists `fockity_dbunit`;
+use fockity_dbunit;
+
 drop table if exists `entity`;
-create table `property` (
-	`id` int unsigned not null auto_increment,
-	`entity` char(50) not null,
-	`property` char(50) not null,
-	unique (`entity`, `property`),
-	key (`entity`),
+create table `entity` (
+	`id` int(10) not null auto_increment,
+	`name` char(25) not null,
+	unique (`name`),
 	primary key (`id`)
 )engine=innodb;
 
-drop table if exists `value`;
-create table `value` (
-	`id` int unsigned not null auto_increment,
-	`property_id` int unsigned not null,
-	`data` text default null,
-	foreign key (`property_id`) references `property` (`id`),
+drop table if exists `property`;
+create table `property` (
+	`id` int(10) not null auto_increment,
+	`entity_id` int(10) not null,
+	`name` char(25) not null,
+	`type` char(3) not null,
+	unique (`entity_id`, `name`),
+	-- foreign key (`entity_id`) references `entity` (`id`),
 	primary key (`id`)
 )engine=innodb;
 
 drop table if exists `record`;
 create table `record` (
-	`id` int unsigned not null auto_increment,
-	`value_id` int unsigned not null,
-	foreign key (`value_id`) references `value` (`id`),
-	primary key (`id`, `value_id`)
+	`id` int(10) not null auto_increment,
+	`entity_id` int(10) not null,
+	-- foreign key (`entity_id`) references `entity` (`id`),
+	primary key (`id`)
 )engine=innodb;
 
-drop table if exists `reference`;
-create table `reference` (
-	`source_id` int unsigned not null,
-	`target_id` int unsigned not null,
-	foreign key (`source_id`) references `record` (`id`),
-	foreign key (`target_id`) references `record` (`id`),
-	primary key (`source_id`, `target_id`)
+
+drop table if exists `value`;
+create table `value` (
+	`id` int(10) not null auto_increment,
+	`property_id` int(10) not null,
+	`record_id` int(10) not null,
+	`value` text not null,
+	-- foreign key (`property_id`) references `property` (`id`),
+	-- foreign key (`record_id`) references `record` (`id`),
+	primary key (`id`)
 )engine=innodb;
